533b79904d05ec7d2a400f6e9790a45d2d82dc7a
Dao Hiep <hieptrang13042002@gmail.com>
2021-09-25 18:01:24 +0700

    style.css, basic layout, javafx-web engine

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Từ điển viết bằng Javafx :)
+
+TODO:
+- giao dien co ban: thanh search, cot results, webview meaning
+- class model cho database: xem nghia cua tu, them tu moi
+- class model goi api cua google
+- style.css
--- src/main/java/dictionary/dictionaryjavafx/DictionaryController.java
+++ src/main/java/dictionary/dictionaryjavafx/DictionaryController.java
@@ -1,14 +1,24 @@
 package dictionary.dictionaryjavafx;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 
-public class DictionaryController {
+public class DictionaryController implements Initializable {
   @FXML
-  private Label welcomeText;
+  Button btnTest;
 
   @FXML
-  protected void onHelloButtonClick() {
-    welcomeText.setText("Welcome to JavaFX Application!");
+  WebView webView;
+
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+    WebEngine webEngine = webView.getEngine();
+    webEngine.loadContent("<h1>abaddon</h1><h3><i>/ə'bædən/</i></h3><h2>danh từ</h2><ul><li>âm ti, địa ngục</li><li>con quỷ</li></ul>");
   }
 }
